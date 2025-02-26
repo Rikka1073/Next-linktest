82bd44ab5cb7bca8b428c1d8cf0187be49bc5a8b
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-26 10:39:26 +0900

    pageのテスト作成

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Page from "../src/app/page";
+
+describe("Page", () => {
+  it("pageがレンダリングされるか", () => {
+    render(<Page />);
+
+    screen.debug();
+  });
+});
