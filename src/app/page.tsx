3c62289a1cd02d94bdf75afe492b92a48533c379
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-26 10:26:13 +0900

    aboutページ作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="text-center">
       <h1 className="text-4xl font-bold">Jestでnext/linkをテストする</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-5 py-2 px-4 rounded">
-        Button
-      </button>
+      <Link href="/about">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-5 py-2 px-4 rounded">
+          Button
+        </button>
+      </Link>
     </div>
   );
 }
